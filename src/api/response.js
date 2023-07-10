b86a941dad7c9d45a50953f1c447ca73fd46492f
HEESUNae <heesun9456@gmail.com>
2023-07-10 19:49:00 +0900

    build test

--- src/api/response.js
+++ src/api/response.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosApi = axios.create({
-  baseURL: 'https://react-blog-heesunae.vercel.appåå',
+  baseURL: 'https://react-blog-heesunae.vercel.app',
   // baseURL: 'http://localhost:4444',
   timeout: 3000,
 });
