a4c29bf34e7f54c3baea1a27792e1a9c717cc27f
HEESUNae <heesun9456@gmail.com>
2023-07-10 17:48:49 +0900

    feat: change env mode

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,12 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
+  require('dotenv').config();
+  const { MODE } = process.env;
+
   app.use(
     createProxyMiddleware('/post/list', {
-      target: 'http://localhost:4444',
+      target: MODE === 'production' ? 'https://react-blog-eight-kappa.vercel.app' : 'http://localhost:4444',
       changeOrigin: true,
     })
   );
