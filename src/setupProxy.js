bc192ffd8c18081d862c502bfd0dc01384d5c348
HEESUNae <heesun9456@gmail.com>
2023-07-11 22:53:58 +0900

    add index proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { PRODUCT_MODE } = process.env;
 
 module.exports = function (app) {
   app.use(
-    createProxyMiddleware(['/post/list', 'user/add'], {
+    createProxyMiddleware(['/', '/post/list', 'user/add'], {
       // target: 'https://react-blog-heesunae.vercel.app',
       // target: BACK_URL,
       target: PRODUCT_MODE === 'develop' ? 'http://localhost:4444' : 'https://react-blog-heesunae.vercel.app',
